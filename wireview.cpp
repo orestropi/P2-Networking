4f42621b0de3d96ca15c22af2ca8ef2df34e492c
orestropi <oropi@wpi.edu>
2021-12-01 19:02:58 -0500

    yay

--- wireview.cpp
+++ wireview.cpp
@@ -60,7 +60,6 @@ void my_callback(u_char *useless, const struct pcap_pkthdr *pkthdr, const u_char
                 inet_ntoa(ip->ip_dst));
               fprintf(stdout,"IP source address: IM IP!"
             );
-
     }
     if (ntohs(eptr->ether_type) == ETHERTYPE_ARP)
     {
